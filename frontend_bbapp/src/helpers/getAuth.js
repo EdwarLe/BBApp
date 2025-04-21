e04baace69b9525818be0898a95b6e587221748d
Edwar León <edwar.leon.m@gmail.com>
2025-04-21 18:38:37 -0500

    Changes submit from github

--- frontend_bbapp/src/helpers/getAuth.js
+++ frontend_bbapp/src/helpers/getAuth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000/api/v1'
+const BASE_URL = 'http://127.0.0.1:8000/api/v1'
 
 export const postAuth = async (endPoint, dataJson) => {
     const uri = BASE_URL + endPoint
